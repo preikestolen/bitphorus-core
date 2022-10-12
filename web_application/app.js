891f6f33b8fc52d925adc972aef37db000b6a0a4
Ahmet Ezdesir <ahmetezdesir.ae@gmail.com>
2022-10-12 13:36:50 +0300

    ejs added

--- web_application/app.js
+++ web_application/app.js
@@ -1,20 +1,23 @@
 const express = require("express");
 const app = express();
+const ejs = require("ejs");
 const port = 3000;
 const bodyParser = require("body-parser");
 // Setting up our static path and Body Parser
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
 
 app.listen(process.env.PORT || port, function() {
     console.log("server is listening the port");
 });
 
 app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/public/home.html");
+    res.render(__dirname + "/views/home.ejs");
 });
 
 app.post("/", function(req, res) {
     const bodyy = req.body;
     console.log(bodyy);
+    res.render(__dirname + "/views/home.ejs", {});
 });
